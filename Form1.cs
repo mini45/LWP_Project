2890b5e3595ffac39b73c42b20f249e44352c5f1
Patrick Meyer <pmeyer@potential-terrorist.com>
2013-06-27 13:46:08 +0200

    Initial Commit

---
+++ Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FlightControll
+{
+    public partial class Form1 : Form
+    {
+        List<Plane> planes = new List<Plane>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Maximized;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.ShowInTaskbar = false;
+            pictureBox1.Image = new Bitmap("C:\\Documents and Settings\\Administrator\\My Documents\\FlightControll\\bg.bmp");
+            //pictureBox1.Size = this.Size;
+            //pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+
+            planes.Add(new Plane(pictureBox1.Width, pictureBox1.Height));
+
+            Timer timer = new Timer();
+            timer.Tick += new EventHandler(Tick);
+            timer.Interval = 100;
+            timer.Start();
+        }
+
+        void Tick(object sender, EventArgs e)
+        {
+            pictureBox1.Invalidate();
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+
+            Graphics g = e.Graphics;
+            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            for (int idx = 0; idx < planes.Count; idx++)
+            {
+                planes[idx].Draw(g);
+            }
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+
+        }
+
+
+    }
+}
---
+++ Plane.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+namespace FlightControll
+{
+    public class Plane
+    {
+        private Bitmap _planeBitmap = new Bitmap("C:\\Documents and Settings\\Administrator\\My Documents\\FlightControll\\plane.png");
+        private float _xSpeed, _ySpeed, _xPos, _yPos, _radius = 20, _angle;
+        private List<Point> _way;
+        private float _maxX, _maxY;
+
+        public Plane(float maxX, float maxY)
+        {
+            this._maxX = maxX;
+            this._maxY = maxY;
+
+            PlaneInit();
+
+        }
+        private void PlaneInit()
+        {
+            Random rnd = new Random();
+
+            //int option = rnd.Next(0, 1);
+            int option = 0;
+            switch (option)
+            {
+                case 0:
+                    this._angle = 0f;
+                    this._xPos = (float)rnd.Next(0, (int)this._maxX - _planeBitmap.Width);
+                    this._yPos = this._maxY;
+                    this._xSpeed = 0f;
+                    this._ySpeed = rnd.Next(2, 4);
+                    break;
+
+
+                default:
+                    break;
+            }
+        }
+
+        public void Draw(Graphics g)
+        {
+            UpdatePos();
+            g.DrawImage(_planeBitmap, _xPos, _yPos, 50, 50);
+        }
+
+        private void UpdatePos()
+        {
+            this._xPos += _xSpeed;
+            this._yPos += _ySpeed;
+        }
+
+        public Point MiddlePoint()
+        {
+            Point middle = new Point((int)_xPos + (int) _planeBitmap.Width / 2,(int) _yPos + _planeBitmap.Height / 2);
+            return(middle);
+        }
+
+        public float Radius
+        {
+            get { return _radius; }
+            set { _radius = value; }
+        }
+
+    }
+}
